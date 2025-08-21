393d5a8a93f303428cac325e9110015886517009
goushino <shinozaki54@gmail.com>
2025-08-21 19:12:24 +0900

    Update: Set up Neovim LSP configs and plug-ins.

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
 local map = vim.keymap.set
 
 map('n', '<Esc><Esc>', ':nohlsearch<CR><Esc>', { silent = true})
@@ -18,4 +24,13 @@ map('n', '.', ':bnext<CR>', { silent = true })
 map('n', 'bd', ':bd<CR>')
 
 map('i', 'jj', '<Esc>', { silent = true })
+
+-- for LSP keymap
+map('n', '<leader>d', vim.diagnostic.open_float, { desc = "LSP diagnostic open float window" })
+map('n', '<leader>a', vim.lsp.buf.code_action, { desc = "LSP code action" })
+map('n', '<leader>rn', vim.lsp.buf.rename, { desc = "LSP rename" })
+map('n', '<leader>rr', vim.lsp.buf.references, { desc = "LSP references" })
+map('n', '<leader>i', vim.lsp.buf.implementation, { desc = "LSP implementation" })
+map('n', '<leader>o', vim.lsp.buf.document_symbol, { desc = "LSP document symbol" })
+map('n', '<leader>t', vim.lsp.buf.type_definition, { desc = "LSP type definition" })
 
